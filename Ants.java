59652e002871aecfee4deb1e7224e967394d5e5c
Robert Young <robertyoung10101@gmail.com>
2017-02-28 11:50:09 +1300

    Compiled the code and imported Scanner.

--- Ants.java
+++ Ants.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
+
 public class Ants{
   public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    System.out.println("Welcome to the NSA.");
+    System.out.print("What do you want to do?");
+    s.nextLine();
+    System.out.println("Mainframe Hacked.");
+    System.out.println("Exiting...");
   }
 }
