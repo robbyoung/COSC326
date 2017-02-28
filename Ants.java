73e7af72300fc8a8e5eb8809561dd98f267a0ca8
Robert Young <robertyoung10101@gmail.com>
2017-02-28 17:06:17 +1300

    Translates DNA input for Etude 1

--- Ants.java
+++ Ants.java
@@ -1,12 +1,59 @@
-import java.util.Scanner;
+/*
+ * @Authors Adam Elder and Robert Young
+ * Etude 1 for COSC326 "Ants"
+ */
+import java.util.*;
 
 public class Ants{
   public static void main(String[] args){
+    ArrayList<int[]> states = new ArrayList<int[]>();
+    ArrayList<String> inputs = new ArrayList<String>();
     Scanner s = new Scanner(System.in);
-    System.out.println("Welcome to the NSA.");
-    System.out.print("What do you want to do?");
-    s.nextLine();
-    System.out.println("Mainframe Hacked.");
-    System.out.println("Exiting...");
+    int temp = 0;
+    while(temp < 2/*s.hasNext()*/){
+      inputs.add(s.nextLine());
+      temp++;
+    }
+    for(int i = 0; i < inputs.size(); i++){
+      Ants.addState(states, inputs.get(i), inputs);
+    }
+    
+    for(int i = 0; i < states.size(); i++){
+      System.out.println(Arrays.toString(states.get(i)));
+    }
+  }
+  
+  public static void addState(ArrayList<int[]> states, String dna, ArrayList<String> inputs){
+    int[] newState = new int[8];
+    newState[0] = Ants.stateToInt(dna.charAt(7), inputs);
+    newState[1] = Ants.stateToInt(dna.charAt(7), inputs);
+    newState[2] = Ants.stateToInt(dna.charAt(7), inputs);
+    newState[3] = Ants.stateToInt(dna.charAt(7), inputs);
+    newState[4] = Ants.convertDirection(dna.charAt(2));
+    newState[5] = Ants.convertDirection(dna.charAt(3));
+    newState[6] = Ants.convertDirection(dna.charAt(4));
+    newState[7] = Ants.convertDirection(dna.charAt(5));
+    states.add(newState);
+  }
+  
+  public static int stateToInt(char state, ArrayList<String> inputs){
+    for(int i = 0; i < inputs.size(); i++){
+      if(inputs.get(i).charAt(0) == state){
+        return i;
+      }
+    }
+    return -1;
+  }
+  
+  public static int convertDirection(char nsew){
+    if(nsew == 'N'){
+      return 0;
+    }else if(nsew == 'E'){
+      return 1;
+    }else if(nsew == 'S'){
+      return 2;
+    }else{
+      return 3;
+    }
   }
 }
