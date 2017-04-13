e80eb1c6e47bcc3ea0eb04a150afbb62003cc7e8
Robert Young <robertyoung10101@gmail.com>
2017-04-13 13:02:11 +1200

    Made the maps report

--- AccessPoints.java
+++ AccessPoints.java
@@ -3,57 +3,122 @@ import java.awt.*;
 import java.awt.geom.Point2D;
 
 public class AccessPoints{
-  public static void main(String[] args){
-    ArrayList<Point2D.Double> points = new ArrayList<Point2D.Double>();
-    Scanner s = new Scanner(System.in);
-    double radius = 10000;
-    while(s.hasNext()){
-      String[] newPoint = s.nextLine().split("\\s+");
-      points.add(new Point2D.Double(Double.parseDouble(newPoint[0]), Double.parseDouble(newPoint[1])));
-    }
-    
-    for(int i = 0; i < points.size(); i++){
-      ArrayList<Point2D.Double> temp = new ArrayList<Point2D.Double>(points);
-      Point2D.Double startingPoint = temp.remove(i);
-      Double tempRadius = irrepressible(radius, startingPoint, temp, points);
-      if(tempRadius < radius){
-        radius = tempRadius;
-      }
-    }
-    System.out.println("Radius: " + radius + " metres.");
-  }
+  public static int circleLimit = 3;
+  public static int numberOfPoints = 15;
+  public static double upperLimit = 10;
+  
+    public static void main(String[] args){
+        for(int p = 0; p < 10; p++){
+            ArrayList<Point2D.Double> points = new ArrayList<Point2D.Double>();
+            Scanner s = new Scanner(System.in);
+            double radius = 10000;
+            /*while(s.hasNext()){
+            String[] newPoint = s.nextLine().split("\\s+");
+            points.add(new Point2D.Double(Double.parseDouble(newPoint[0]), Double.parseDouble(newPoint[1])));
+            }*/
+            double maxX = upperLimit, maxY = upperLimit, minX = 0, minY = 0;
+            Random r = new Random();
+            for(int k = 0; k < numberOfPoints; k++){
+                double randomX = minX + (maxX - minX) * r.nextDouble();
+                double randomY = minY + (maxY - minY) * r.nextDouble();
+                points.add(new Point2D.Double(randomX, randomY));
+            }
+            System.out.println(points.toString());
 
-  public static double irrepressible(double radius, Point2D.Double midpoint, ArrayList<Point2D.Double> points, ArrayList<Point2D.Double> allPoints){
-    int pointCount = 0;
-    double max = 0;
-    boolean failed;
-    do{
-      failed = false;
-      for(int i = 0; i < allPoints.size(); i++){
-        double distance = Math.abs(midpoint.distance(allPoints.get(i)));
-        if(distance <= radius){
-          pointCount++;
-          if(max < distance){
-            max = distance;
-          }
+            for(int i = 0; i < points.size(); i++){
+                ArrayList<Point2D.Double> temp = new ArrayList<Point2D.Double>(points);
+                Point2D.Double startingPoint = new Point2D.Double(temp.get(i).x, temp.get(i).y);
+                temp.remove(i);
+                Double tempRadius = irrepressible(radius, startingPoint, temp, points, i, 1);
+                if(tempRadius < radius){
+                    radius = tempRadius;
+                }
+            }
+            System.out.println("Radius: " + radius + " metres.");
+            is_irrepressible(radius, points);
+            System.out.println();
+            System.out.println();
         }
-        if(pointCount >= 12){
-          radius = max - 0.01;
-          failed = true;
-        }
-      }
-    }while(failed);
-    
-    for(int i = 0; i < points.size(); i++){
-      ArrayList<Point2D.Double> temp = new ArrayList<Point2D.Double>(points);
-      Point2D.Double startingPoint = temp.remove(i);
-      startingPoint.x = (startingPoint.x + midpoint.x)/2;
-      startingPoint.y =(startingPoint.y + midpoint.y)/2;
-      Double tempRadius = irrepressible(radius, null, temp, allPoints);
-      if(tempRadius < radius){
-        radius = tempRadius;
-      }
     }
-    return radius;
-  }
+
+    public static double irrepressible(double radius, Point2D.Double midpoint, ArrayList<Point2D.Double> points, ArrayList<Point2D.Double> allPoints, int index, int depth){
+        //System.out.println("Level " + points.size());
+        //System.out.println("Midpoint: (" + midpoint.x + ", " + midpoint.y + ")");
+        //System.out.println(allPoints.toString());
+        int pointCount = 0;
+        double max = 0;
+        //System.out.println(points.toString());
+        //System.out.println(allPoints.toString());
+        for(int i = 0; i < allPoints.size(); i++){
+            double distance = Math.abs(midpoint.distance(allPoints.get(i)));
+            //System.out.println("Distance: " + distance + " / Radius: " + radius);
+            if(distance <= radius){
+                pointCount++;
+                if(max <= distance){
+                    max = distance;
+                    //System.out.println("Max distance: " + max);
+                }
+            }
+            if(pointCount >= circleLimit){
+                radius = Math.round((max) * 100)/100.00 - 0.01;
+                //System.out.println("Radius: " + radius + " metres.");
+                i = -1;
+                pointCount = 0;
+                max = 0;
+            }
+        }
+
+        for(int i = index; i < points.size(); i++){
+            ArrayList<Point2D.Double> temp = new ArrayList<Point2D.Double>(points);
+            Point2D.Double startingPoint = new Point2D.Double(temp.get(i).x, temp.get(i).y);
+            temp.remove(i);
+            startingPoint.x = ((startingPoint.x * depth) + midpoint.x)/(depth + 1);
+            startingPoint.y =((startingPoint.y * depth) + midpoint.y)/(depth + 1);
+            Double tempRadius = irrepressible(radius, startingPoint, temp, allPoints, i, depth+1);
+            if(tempRadius < radius){
+                radius = tempRadius;
+            }
+        }
+        return radius;
+    }
+
+    public static void is_irrepressible(double radius, ArrayList<Point2D.Double> points){
+        double maxX = points.get(0).x, maxY = points.get(0).y, minX = maxX, minY = maxY;
+        for(int i = 0; i < points.size(); i++){
+            Point2D.Double current = points.get(i);
+            if(maxX < current.x){
+                maxX = current.x;
+            }
+            if(minX > current.x){
+                minX = current.x;
+            }
+            if(maxY < current.y){
+                maxY = current.y;
+            }
+            if(minY > current.y){
+                minY = current.y;
+            }
+        }
+        Random r = new Random();
+        for(int k = 0; k < 100000; k++){
+            double randomX = minX + (maxX - minX) * r.nextDouble();
+            double randomY = minY + (maxY - minY) * r.nextDouble();
+            Point2D.Double current = new Point2D.Double(randomX, randomY);
+            int pointCount = 0;
+            double max = 0;
+            for(int i = 0; i < points.size(); i++){
+                double distance = Math.abs(current.distance(points.get(i)));
+                if(distance <= radius){
+                    pointCount++;
+                    if (distance > max){
+                        max = distance;
+                    }
+                }
+            }
+            if(pointCount >= circleLimit){
+                System.out.println("Exception: " + current.x + ", " + current.y + " --> " + max + " metres, " + pointCount + " points.");
+            }
+        }
+        System.out.println("Tests Complete");
+    }
 }
--- HeadsAndTails.java
+++ HeadsAndTails.java
@@ -1,8 +1,22 @@
+/*
+ * @authors Adam Elder and Robert Young
+ * Etude 11 Solution for COSC326
+ */
 import java.util.*;
 
+/*
+ * Given the number of heads followed by the number of tails, finds a way
+ *  to move the coins to make an alternating pattern.
+ */
 public class HeadsAndTails{
+  
+  /* 
+   * Reads the number of heads and tails from stdin, outputs every step of
+   *  the solution to stdout.
+   */
   public static void main(String[] args){
     Scanner s = new Scanner(System.in);
+    //Do this for every coin scenario given.
     while(s.hasNext()){
       String[] input = s.nextLine().split("\\s+");
       int numHeads = Integer.parseInt(input[0]);
@@ -10,6 +24,7 @@ public class HeadsAndTails{
       ArrayList<String> coins = new ArrayList<String>();
       String starter;
       int steps = 0;
+      //Set up the initial coin sequence.
       if(numTails > numHeads){
         starter = "T";
       }else{
@@ -25,12 +40,14 @@ public class HeadsAndTails{
       }
       printCoins(coins);
       
+      //Get the appropriate coin type to the start of the sequence.
       while(!starter.equals(coins.get(0))){
         fixStart(coins);
         printCoins(coins);
         steps++;
       }
       
+      //Find a way to fix double ups until there are no more.
       while(!swapCoins(coins)){
         printCoins(coins);
         steps++;
@@ -39,6 +56,11 @@ public class HeadsAndTails{
     }
   }
   
+  /*
+   * This method moves the coin type of greater number to the start of the 
+   *  sequence of coins.
+   * @param coins is an ArrayList of "H" and "T" strings.
+   */
   public static void fixStart(ArrayList<String> coins){
     for(int i = 0; i < coins.size(); i++){
       if(coins.get(i).equals("T")){
@@ -48,6 +70,11 @@ public class HeadsAndTails{
     }
   }
   
+  /*
+   * This method finds the leftmost point where coins can be swapped to fix a
+   *  double-up and then swaps the coins at that point.
+   * @param coins is an ArrayList of "H" and "T" strings.
+   */
   public static boolean swapCoins(ArrayList<String> coins){
     for(int i = 1; i < coins.size() - 1; i++){
       if(coins.get(i).equals(coins.get(i-1)) && !coins.get(i).equals(coins.get(i+1))){
@@ -58,6 +85,10 @@ public class HeadsAndTails{
     return true;
   }
   
+  /*
+   * This method prints out a coin sequence represented by an ArrayList.
+   * @param coins is an ArrayList of "H" and "T" strings.
+   */
   public static void printCoins(ArrayList<String> coins){
     String line = "";
     for(int i = 0; i < coins.size(); i++){
